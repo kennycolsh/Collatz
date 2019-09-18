3529a37a81f16dc7d02762cda4ce6a0e5b44f40c
Kenny Colsh <kennycolsh@yahoo.com>
2019-09-18 13:38:43 +0100

    added read me

--- collatz.py
+++ collatz.py
@@ -1,10 +1,10 @@
-n = 20
+n = int(input("enter a positive number "))
 #the number we will use
-while n ! =1:
+while n !=1:
     print(n)
     #checks if a number is odd or even.
     if n % 2 == 0:
-        n = n / 2
+        n = n // 2
     else:
         n = (3 * n) + 1
 
